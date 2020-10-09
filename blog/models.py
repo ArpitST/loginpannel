8aed62fe8d9fb1605e9ce74411bc144962896b05
ArpitST <arpit@securitytroops.com>
2020-10-09 16:54:20 +0530

    twelth commit

--- blog/models.py
+++ blog/models.py
@@ -23,7 +23,7 @@ class Post(models.Model):
     author=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title=models.CharField(max_length=200)
     thumbnail_Image=models.ImageField(upload_to="media")
-    # featured_Image=models.ImageField(upload_to="media", default='')
+    featured_Image=models.ImageField(upload_to="media", default='')
     text=models.TextField()
     created_date=models.DateTimeField(default=timezone.now)
     published_date=models.DateTimeField(blank=True, null=True)
